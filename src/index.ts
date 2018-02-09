cb16e1fc7cf85d681e548578ba67178e462d16b3
toxa16 <toxa16@gmail.com>
2018-02-09 19:16:17 +0200

    export injector

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as HttpMethod } from './http-method';
 export { default as Endpoint } from './endpoint';
+export { default as Injector } from './injector';
 
 export {
   Exception, HttpError, MethodNotAllowed, NotFound, NotImplemented,
